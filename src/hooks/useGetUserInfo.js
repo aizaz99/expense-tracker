f73acc67edac3a1ee54cdff93d6ff206213396fe
Aizaz Ahmad <aizazahmed476@gmail.com>
2024-01-16 21:34:54 -0800

    first commit

---
+++ src/hooks/useGetUserInfo.js
@@ -0,0 +1,6 @@
+export const useGetUserInfo = () => {
+    const {name,profilePhoto,userID, isAuth} = JSON.parse(localStorage.getItem("auth")) //Transforms stringify back to objec
+return {name, profilePhoto, userID, isAuth}
+
+}
+
