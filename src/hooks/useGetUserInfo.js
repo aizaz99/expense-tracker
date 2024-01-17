92b5f55593ab890e7b38e1cdc3144ebfaa3e1247
Aizaz Ahmad <aizazahmed476@gmail.com>
2024-01-16 22:14:43 -0800

    fixes

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,5 +1,5 @@
 export const useGetUserInfo = () => {
-    const {name,profilePhoto,userID, isAuth} = JSON.parse(localStorage.getItem("auth")) //Transforms stringify back to objec
+    const {name,profilePhoto,userID, isAuth} = JSON.parse(localStorage.getItem("auth")) || {};//Transforms stringify back to objec
 return {name, profilePhoto, userID, isAuth}
 
 }
